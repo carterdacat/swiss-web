ed178a9af43d06a5634f00e4c619f2853d406b48
Carter Nabors <carter@Carters-MacBook-Pro.local>
2020-03-13 20:57:27 -0400

    Adds a simple feature.

---
+++ src/home.js
@@ -0,0 +1,32 @@
+import React, {useState} from 'react';
+import './home.css';
+import logo from "./image.png"
+function Homepg() {
+
+  const [speed, setSpeed] = useState(15);
+
+  function faster () {
+    setSpeed(speed === 1 ? speed : speed-2)
+  };
+
+  function slower() {
+    setSpeed(speed+2);
+  }
+
+  const animationStyle = {
+    animation: `App-logo-spin infinite ${speed}s linear`
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} style={animationStyle} className="App-logo-circle" alt="White cross, with a blue bacground spining" id='spinnerLogo'/>
+        <p>Hello, and welcome to the begining of the Swiss Plus Website. <strong>We hope you enjoy your stay</strong></p>
+        <button className='App-button' id='fastLogoButton' onClick={faster}>Increase Spin Speed!</button>
+        <button className='App-button' id='slowLogoButton' onClick={slower}>Decrease Spin Speed!</button>
+      </header>
+    </div>
+  );
+}
+
+export default Homepg;
